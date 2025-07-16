473813d86993948620fc480a14af4a149b885e93
Weikai Kong <priv@pppig236.com>
2025-07-15 21:06:08 -0400

    nuke particles

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import App from './App.vue'
 import router from './router'
 import VueGtag from 'vue-gtag-next'
 import vuetify from './plugins/vuetify'
-import Particles from 'particles.vue3'
 import { loadFonts } from './plugins/webfontloader'
 
 loadFonts()
@@ -17,5 +16,4 @@ createApp(App)
     }
   })
   .use(vuetify)
-  .use(Particles)
   .mount('#app')
